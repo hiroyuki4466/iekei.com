2d10ff26bf2b000677659101e8626f5e0aa65f56
hiroyuki_takei <hiro@hiroyuki-takeinoMacBook-Air.local>
2022-07-28 10:41:59 +0900

    デプロイ前

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   belongs_to :user
   
   validates :visited_date, presence: true
+  validates :shop_name, presence: true, length: {maximum:50}
+  validates :shop_memo, presence: true, length: {maximum:300}
+  validates :shop_score, presence: true, numericality: {only_integer: true}
+  validates :address, presence: true
 end
