8569393c4a1482b9f5c941fd8c7f3f30b5cbd814
hiroyuki_takei <hiro@hiroyuki-takeinoMacBook-Air.local>
2022-08-30 18:07:51 +0900

    UI/UX微調整

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'   
-}
+  }
 
   devise_scope :user do
     get "user/:id", :to => "users/registrations#detail"
